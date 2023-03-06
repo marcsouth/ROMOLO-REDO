e8de28a4954508b81d314ef733db2783bf022be0
marcsouth <104800775+marcsouth@users.noreply.github.com>
2023-03-06 05:10:55 -0500

    ARTWORK SECTION STARTED

--- src/pages/artworks.js
+++ src/pages/artworks.js
@@ -1,23 +1,20 @@
 import Navigation from "../components/navigation"
+import { ArtworkData } from "../utils/data"
 
 export default function Artworks() {
   return (
     <>
     <Navigation />
-
-
- <div className="artgrid">
-
-
-      
-
-
-
-
-
- </div>
-
+    <div className="artwork-gallery">
+        <div className="artgrid">
+          {ArtworkData.map((artworkdata) => (
+            <div key={artworkdata.id} className="artgrid-item">
+              <img src={artworkdata.src} alt={artworkdata.title} />
+            </div>
+          ))}
+        </div>
+      </div>
     </>
-  )
+  );
 }
  
---
+++ src/utils/data.js
@@ -0,0 +1,64 @@
+export const ArtworkData = [
+
+{
+    id:1,
+    src:"../Artwork photos/photo1.jpg",
+    title:"Daedelus Sculpture in Bronze"
+},
+{
+    id:2,
+    src:"../Artwork photos/photo2.jpg",
+    title:"Daedelus Sculpture in Bronze"
+},
+{
+    id:3,
+    src:"../Artwork photos/photo3.jpg",
+    title:"Daedelus Sculpture in Bronze"
+},
+{
+    id:4,
+    src:"../Artwork photos/photo4.jpg",
+    title:"Daedelus Sculpture in Bronze"
+},
+{
+    id:5,
+    src:"../Artwork photos/photo5.jpg",
+    title:"Calliope Sculpture in Bronze"
+},
+{
+    id:6,
+    src:"../Artwork photos/photo6.jpg",
+    title:"Calliope Sculpture in Bronze"
+},
+{
+    id:7,
+    src:"../Artwork photos/photo7.jpg",
+    title:"Calliope Sculpture in Bronze"
+},
+{
+    id:8,
+    src:"../Artwork photos/photo8.jpg",
+    title:"Terra, Aqua, Fuco in Bronze"
+},
+{
+    id:9,
+    src:"../Artwork photos/photo9.jpg",
+    title:"The Tree of Life Which Is Ours in Bronze"
+},
+{
+    id:10,
+    src:"../Artwork photos/photo10.jpg",
+    title:"The Tree of Life Which Is Ours in Bronze"
+},
+{
+    id:11,
+    src:"../Artwork photos/photo11.jpg",
+    title:"The Tree of Life Which Is Ours in Bronze"
+},
+{
+    id:12,
+    src:"../Artwork photos/photo12.png",
+    title:"The Tree of Life Which Is Ours in Bronze"
+},
+
+];
